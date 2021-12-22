66f685e0bd3dc514e8f6124e66e2dcf96e411522
Eng.Njokosi <eng.njokosi@gmail.com>
2021-12-22 21:05:26 +0300

    :wrench: Update tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,53 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
+  mode: "jit",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/templates/**/*.{js,ts,jsx,tsx}",
+    "./src/layout/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Inter",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+        serif: ["Georgia", "Cambria", '"Times New Roman"', "Times", "serif"],
+        mono: [
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          '"Liberation Mono"',
+          '"Courier New"',
+          "monospace",
+        ],
+      },
+      colors: {
+        "light-yellow": "#F6F3EC",
+        orange: colors.orange,
+        teal: colors.teal,
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
