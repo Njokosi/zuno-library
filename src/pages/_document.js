110807102ce5d6db768e9f939bcce9c603edd8d5
Eng.Njokosi <eng.njokosi@gmail.com>
2021-12-23 18:55:02 +0300

    :zap: Update manifest for PWA

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="images/icons/icon-96x96.png" />
+          <meta name="apple-mobile-web-app-status-bar" content="#2196f3" />
+          <meta name="theme-color" content="#2196f3" />
+        </Head>
         <body>
           <Main />
           <NextScript />
