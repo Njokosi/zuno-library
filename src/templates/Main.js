2aabcd756ecb41861b8cc4e1fd19a41dc473a700
Eng.Njokosi <eng.njokosi@gmail.com>
2021-12-23 19:12:30 +0300

    :zap: Update the layout design

--- src/templates/Main.js
+++ src/templates/Main.js
@@ -5,18 +5,9 @@ import Link from "next/link";
 
 export const Main = (props) => {
   return (
-    <div className="w-full px-1 antialiased text-gray-700 bg-white">
+    <div className="w-full overflow-hidden antialiased bg-white">
       {props.meta}
-      <div className="overflow-hidden bg-light-yellow">
-        <div className="mx-auto max-w-7xl">
-          {/* <Navbar />
-          <div className="w-full bg-light-yellow">
-            <SearchBar />
-            <Hero />
-          </div> */}
-        </div>
-      </div>
-      <div className="text-xl content">{props.children}</div>
+      <div className="content">{props.children}</div>
       {/* <Footer /> */}
     </div>
   );
