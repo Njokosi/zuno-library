bc2f63334ef7c6ce3a73e03c588a8868d20eeef0
Eng.Njokosi <eng.njokosi@gmail.com>
2021-12-23 18:53:55 +0300

    🔧 Fix tailwind css files loading

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/main.css'
+import "tailwindcss/tailwind.css";
+import "../styles/main.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
